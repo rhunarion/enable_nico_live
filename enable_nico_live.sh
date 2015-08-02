4e24b5aedfd21fe9bbed2da3025c6bd763300bb5
rhunarion <akira.kako@gmail.com>
2015-08-02 19:23:21 +0900

    Update enable_nico_live.sh

--- enable_nico_live.sh
+++ enable_nico_live.sh
@@ -7,12 +7,12 @@ function change_klimit_value() {
     python s2x.py -x $DOMAIN/settings.sol
     sed -i "s/<data name=\"klimit\" type=\"number\" value=\".\?\"\/>/<data name=\"klimit\" type=\"number\" value=\"-2.0\"\/>/g" $DOMAIN/settings.xml
     python s2x.py -s $DOMAIN/settings.xml
-    rm -i $DOMAIN/settings.xml
+    rm -v $DOMAIN/settings.xml
 }
 
 cd $DIRECTORY
 wget https://raw.githubusercontent.com/rhunarion/enable_nico_live/master/s2x.py
 DOMAIN=\#live.nicovideo.jp change_klimit_value
 DOMAIN=\#res.nimg.jp change_klimit_value
-rm -is 2x.py
+rm -v s2x.py
 cd $CUR_DIR
