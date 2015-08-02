4c24e677994e719c2babf27671dedfea60c99be0
rhunarion <akira.kako@gmail.com>
2015-08-02 19:22:01 +0900

    Update enable_nico_live.sh

--- enable_nico_live.sh
+++ enable_nico_live.sh
@@ -5,14 +5,14 @@ DIRECTORY=~/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
 
 function change_klimit_value() {
     python s2x.py -x $DOMAIN/settings.sol
-    sed -i "s/<data name=\"klimit\" type=\"number\" value=\"100.0\"\/>/<data name=\"klimit\" type=\"number\" value=\"-2.0\"\/>/g" $DOMAIN/settings.xml
+    sed -i "s/<data name=\"klimit\" type=\"number\" value=\".\?\"\/>/<data name=\"klimit\" type=\"number\" value=\"-2.0\"\/>/g" $DOMAIN/settings.xml
     python s2x.py -s $DOMAIN/settings.xml
-    rm $DOMAIN/settings.xml
+    rm -i $DOMAIN/settings.xml
 }
 
 cd $DIRECTORY
 wget https://raw.githubusercontent.com/rhunarion/enable_nico_live/master/s2x.py
 DOMAIN=\#live.nicovideo.jp change_klimit_value
 DOMAIN=\#res.nimg.jp change_klimit_value
-rm s2x.py
+rm -is 2x.py
 cd $CUR_DIR
