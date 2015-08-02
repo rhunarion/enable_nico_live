dbd46c3cc1406d58087be5d3079556230a1460ff
rhunarion <akira.kako@gmail.com>
2015-08-02 19:54:17 +0900

    Update enable_nico_live.sh

--- enable_nico_live.sh
+++ enable_nico_live.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-CUR_DIR=`pwd`
-DIRECTORY=~/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
-
 function change_klimit_value() {
     python s2x.py -x $DOMAIN/settings.sol
     sed -i "s/<data name=\"klimit\" type=\"number\" value=\".*\"\/>/<data name=\"klimit\" type=\"number\" value=\"-2.0\"\/>/g" $DOMAIN/settings.xml
     python s2x.py -s $DOMAIN/settings.xml
     rm -v $DOMAIN/settings.xml
 }
+
+CUR_DIR=`pwd`
+DIRECTORY=~/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
 
 cd $DIRECTORY
 wget https://raw.githubusercontent.com/rhunarion/enable_nico_live/master/s2x.py
